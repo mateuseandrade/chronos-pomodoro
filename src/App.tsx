674f77b7fef7ec65e8e163f87d5de464e4db36ea
Mateus Andrade <mateuseandrade@gmail.com>
2025-05-09 22:18:10 -0300

    container para react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        neque et cum cupiditate dicta possimus ea eaque. Fugiat sed mollitia
-        reiciendis beatae laborum sit vero, ex excepturi, ad fugit magni.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
