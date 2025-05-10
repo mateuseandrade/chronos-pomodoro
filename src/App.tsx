2b9a0738c6b24686b0901be31b403c427ed3a21b
Mateus Andrade <mateuseandrade@gmail.com>
2025-05-09 21:45:53 -0300

    componente heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading> Olá Mundo</Heading>
+      <Heading>
+        Olá Mundo
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
         neque et cum cupiditate dicta possimus ea eaque. Fugiat sed mollitia
